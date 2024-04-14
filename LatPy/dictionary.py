31da794cf03c8158660dd43246af7b8eb8b2c68a
dimasdaffa <dimas.de2004@gmail.com>
2024-04-14 22:10:51 +0700

    interaksi data

---
+++ LatPy/dictionary.py
@@ -0,0 +1,67 @@
+"""
+TODO:
+Buatlah variabel dictionary dengan nama "data_diri",
+variabel tersebut berisi identitas diri Anda berdasarkan ketentuan berikut.
+- Memiliki key bernama "firstName":
+    - Isi value dengan nama depan Anda, pastikan bertipe data string.
+- Memiliki key bernama "lastName":
+    - Isi value dengan nama terakhir Anda, pastikan bertipe data string.
+- Memiliki key bernama "age":
+    - Isi value dengan umur Anda, pastikan bertipe data integer.
+- Memiliki key bernama "isMarried":
+    - Isi value dengan status pernikahan Anda, pastikan bertipe data boolean.
+
+Catatan:
+- Value pada dictionary harus berupa nilai sesungguhnya (literal) seperti string, 
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+
+# TODO: Silakan buat kode Anda di bawah ini.
+"""
+TODO:
+
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string,
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+"""
+TODO:
+
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string,
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+
+firstName = "Dimas"
+lastName = "Daffa"
+age = 19
+isMarried = False
+# TODO: Silakan buat kode Anda di bawah ini.
+
+data_diri = {
+    "firstName": "Dimas",
+    "lastName": "Daffa",
+    "age": 19,
+    "isMarried": False
+}
+
+print(data_diri)
---
+++ LatPy/tipeData.py
@@ -0,0 +1,42 @@
+"""
+TODO:
+
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string,
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+"""
+TODO:
+
+Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+
+- firstName: isi dengan nama depan Anda bertipe data string.
+- lastName: isi dengan nama belakang Anda bertipe data string.
+- age: isi dengan umur Anda bertipe data integer.
+- isMarried: isi dengan status pernikahan Anda bertipe data boolean.
+
+Catatan:
+- Value variabel harus berupa nilai sesungguhnya (literal) seperti string,
+  bilangan bulat (integer), dan boolean (benar atau salah).
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+
+firstName = "Dimas"
+lastName = "Daffa"
+age = 19
+isMarried = False
+
+print("Nama Depan:", firstName)
+print("Nama Belakang:", lastName)
+print("Umur:", age)
+print("Status Menikah:", isMarried)
